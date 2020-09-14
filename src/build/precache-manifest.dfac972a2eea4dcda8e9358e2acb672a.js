e3bf074b151087e4e84bc7c39956cb0056fde527
Ali Rajabali Nejad <atin.ariamehr@gmail.com>
2020-09-14 16:43:27 -0400

    first commit

---
+++ src/build/precache-manifest.dfac972a2eea4dcda8e9358e2acb672a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d740134889e920c1c5257b960a884952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4360d1f5e6b2ceceaa",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "fa69ca602fb6172b8e1f",
+    "url": "/static/js/2.ab2a2423.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab2a2423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4360d1f5e6b2ceceaa",
+    "url": "/static/js/main.874eca1d.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
